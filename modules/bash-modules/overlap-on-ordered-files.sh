716272fb0d95853517843abadb10bd0aaa7106a0
Jesper Gaadin <jesgaa@computerome01.cm.cluster>
2019-12-17 19:28:28 +0100

    preparing for the new bioc getter for allele information

--- modules/bash-modules/overlap-on-ordered-files.sh
+++ modules/bash-modules/overlap-on-ordered-files.sh
@@ -1,4 +1,5 @@
 file1=$1
 file2=$2
 
-join -t $'\t' -1 1 -2 1 <(cat ${file1}) <(gzip -dc ", ${file2}," ) 
+#join -t $'\t' -1 1 -2 1 <(cat ${file1}) <(gzip -dc ${file2} ) 
+gzip -dc ${file2} | join -t $'\t' -1 1 -2 1 ${file1} -
