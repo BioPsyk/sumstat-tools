c87889f35b62057d71ef000742f747fe7e6760be
pappewaio <j.r.gadin@gmail.com>
2019-12-23 23:16:30 +0100

    line by line allele modifier calculator

--- modules/R-modules/allele-matching-to-reference.R
+++ modules/R-modules/allele-matching-to-reference.R
@@ -1,46 +1,78 @@
-# To simplify subsetting, collect vectors in data.frame
-df <- data.frame(A1=A1, A2=A2, ref=ref ,alt=alt, stringsAsFactors=FALSE)
+args = commandArgs(trailingOnly=TRUE)
 
-# Add some NAs
-df$A1[c(4,234,4534)] <- NA
+whichinx=args[1]
+#whichinx="a1=1,a2=2,d1=4,d2=5"
+spl <- unlist(strsplit(whichinx, ","))
+spl2 <- strsplit(spl, "=")
+args <- unlist(lapply(spl2,FUN=function(x){x[1]}))
+vals <- as.integer(unlist(lapply(spl2,FUN=function(x){x[2]})))
+names(vals) <- args
+cix=c(vals[["a1"]], vals[["a2"]], vals[["d1"]], vals[["d2"]])
 
-# Add some nucleotides not within the set of A, T, G or C
-df$A2[c(10,544,3234)] <- c("X","PO","?")
+input<-file('stdin', 'r')
+#line <- c("A","C","ix","C","A")
+#line <- paste(line, collapse="\t")
+while(length(line <- readLines(input, n=1)) > 0){
+  el <- unlist(strsplit(line, "\t"))[cix]
+  
+  # To simplify subsetting, collect vectors in data.frame
+  #df <- data.frame(A1=A1, A2=A2, ref=ref ,alt=alt, stringsAsFactors=FALSE)
+  
+  # Check for NA 
+  tf <- is.na(el[1]) | is.na(el[2]) | is.na(el[3]) | is.na(el[4])
+  if(tf){
+    write(paste(line, "NAs", sep="\t"), stderr())
+    next
+  }
 
-# Check for NA 
-tf <- is.na(df$A1) | is.na(df$A2) | is.na(df$ref) | is.na(df$alt)
-df <- df[!tf,]
-
-# Check for nucleotide not in A, T, G or C
-tf0 <- (!(df$A1 %in% c("A","T","G","C"))) | (!(df$A2 %in% c("A","T","G","C"))) | (!(df$ref %in% c("A","T","G","C"))) | (!(df$alt %in% c("A","T","G","C")))
-df <- df[!tf0,]
-
-# Check for homozygote variants (should not happen in real data)
-tf00 <- df$A1 == df$A2
-df <- df[!tf00,]
-tf000 <- df$ref == df$alt
-df <- df[!tf000,]
-
-# Apply palindrom-filter
-tf1 <- .calc_palindrom(df$A1,df$A2)
-tf2 <- .calc_palindrom(df$ref, df$alt)
-df <- df[!(tf1|tf2),]
-
-# Calculate opposite strand eqivalent
-df$B1 <- .opposite_strand(df$A1)
-df$B2 <- .opposite_strand(df$A2)
-
-# Check for not expected A2:s. In real data we should have very few non expected A2s (in any case, exclude them)
-tf3 <- .not_expected_a2(df$A1, df$A2, df$B1, df$B2, df$ref, df$alt)
-df <- df[!tf3,]
-
-# Find the only logically possible pairs, all other are excluded (should not be non-possible pairs in real data)
-tf4 <- .db_possible_pairs(df$A1, df$A2, df$B1, df$B2, df$ref, df$alt)
-df <- df[tf4,]
-
-# Now we are ready to calculate the effect modifier (assumes all filters above have been applied)
-df$em <- .effect_modifier(df$A1, df$B1, df$ref)
-
-# Check distribution
-table(df$em)
+  # Check for nucleotide not in A, T, G or C
+  tf <- (!(el[1] %in% c("A","T","G","C"))) | (!(el[2] %in% c("A","T","G","C"))) | (!(el[3] %in% c("A","T","G","C"))) | (!(el[4] %in% c("A","T","G","C")))
+  if(tf){
+    write(paste(line, "notGCTA", sep="\t"), stderr())
+    next
+  }
+  # Check for homozygote variants (should not happen in real data)
+  tf <- el[1] == el[2]
+  if(tf){
+    write(paste(line, "hom", sep="\t"), stderr())
+    next
+  }
+  tf <- el[3] == el[4]
+  if(tf){
+    write(paste(line, "hom", sep="\t"), stderr())
+    next
+  }
+  
+  # Apply palindrom-filter
+  tf1 <- .calc_palindrom(el[1],el[2])
+  tf2 <- .calc_palindrom(el[3], el[4])
+  if(tf1|tf2){
+    write(paste(line, "palindrom", sep="\t"), stderr())
+    next
+  }
+  
+  # Calculate opposite strand eqivalent
+  B1 <- .opposite_strand(el[1])
+  B2 <- .opposite_strand(el[2])
+  
+  # Check for not expected A2:s. In real data we should have very few non expected A2s (in any case, exclude them)
+  tf3 <- .not_expected_a2(el[1], el[2], B1, B2, el[3], el[4])
+  if(tf3){
+    write(paste(line, "notExpA2", sep="\t"), stderr())
+    next
+  }
+  
+  # Find the only logically possible pairs, all other are excluded (should not be non-possible pairs in real data)
+  tf4 <- .db_possible_pairs(el[1], el[2], B1, B2, el[3], el[4])
+  if(!tf4){
+    write(paste(line, "notPossible", sep="\t"), stderr())
+    next
+  }
+  
+  # Now we are ready to calculate the effect modifier (assumes all filters above have been applied)
+  em <- .effect_modifier(el[1], B1, el[3])
+  
+  # write to stdout the line that survived all filters and got a modifier
+  write(paste(el[3], el[4], em, sep="\t"), stdout())
+}
 
